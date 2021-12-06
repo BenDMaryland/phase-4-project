61917e75a2fe763cb91969f46e6d68d4a7de4256
My Name <ben.d.maryland@gmail.com>
2021-12-06 10:33:19 -0500

    testing models

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,6 +2,10 @@ class Car < ApplicationRecord
     has_many :reviews 
     has_many :users, through: :reviews
 
+belongs_to :owned_by, class_name: "User", foreign_key: "owned_by_id"
+belongs_to :photographed_by, class_name: "User", foreign_key: "photographed_by_id", optional: true
+
+
     validates :photo, presence: true 
     validates :description, presence: true 
     validates :model, presence: true 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :cars, through: :reviews 
 
+has_many :cars_owned, class_name: "Car", foreign_key: "owned_by_id"
+has_many :photographs_taken, class_name: "Car", foreign_key: "photographed_by_id"
+
+
+
     validates :name, presence: true 
     validates :email, presence: true 
     validates :password, presence: true 
