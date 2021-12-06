121723a1c573d2fbf213b8a78f8649fb80080f69
My Name <ben.d.maryland@gmail.com>
2021-12-06 11:38:13 -0500

    relationships created and tested

--- db/migrate/20211205003515_create_cars.rb
+++ db/migrate/20211205003515_create_cars.rb
@@ -1,8 +1,8 @@
 class CreateCars < ActiveRecord::Migration[6.1]
   def change
     create_table :cars do |t|
-      t.integer :owned_by, foreign_key: {to_table: "users"}
-      t.integer :photographed_by, foreign_key: {to_table: "users"}
+      t.references :owned_by, foreign_key: {to_table: "users"}
+      t.references :photographed_by, foreign_key: {to_table: "users"}
       t.string :photo
       t.string :description
       t.string :model
