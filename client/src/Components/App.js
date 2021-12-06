ee58214ebefbe704777c043996cf877c184efbac
My Name <ben.d.maryland@gmail.com>
2021-12-06 16:27:12 -0500

    put users functions in app/ added signup

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -2,20 +2,104 @@ import './App.css';
 import TopNav from './TopNav';
 import About from './About';
 import SignupForm from './SignupForm';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  //  This Usestate is for the current user who is loggedin
+  const [user, setuser] = useState("")
+  // This Use state is loggin in. 
+  const [loginDetails, setloginDetails] = useState({
+    email: "",
+    password: ""
+  })
+
+const [newUser, setnewUser] = useState({
+  name: "",
+  password: "",
+  email: "",
+  photographer: ""
+})
+
+
+  // for chaning the form to signup 
+  function newUserChangeHanldler(e) {
+    setnewUser(data => data = { ...data, [e.target.name]: e.target.value })
+ 
+  }
+
+  //submitting new user requests
+  function newUserSubmitHandler(e) {
+    e.preventDefault()
+    console.log(e)
+    console.log(newUser)
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: newUser.email,
+        password: newUser.password,
+        name: newUser.name,
+        photographer: newUser.photographer
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setuser(user));
+      }
+    });
+  }
+
+
+
+// for chaning the form to login 
+  function changeHanldler(e) {
+    setloginDetails(data => data = { ...data, [e.target.name]: e.target.value })
+  }
+
+ //submitting login requests
+  function submitHandler(e) {
+    e.preventDefault()
+    console.log(e)
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: loginDetails.email,
+        password: loginDetails.password
+
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setuser(user));
+      }
+    });
+  }
+
+
+
+
+
+
+
   return (
+
+
+
     <div className="App">
-      <TopNav />
+      <TopNav  changeHanldler={changeHanldler}  submitHandler={submitHandler} loginDetails={loginDetails} user={user}/>
       <Routes >
-        <Route  path="/about/*" element={<About />} />
-        <Route path="/signup/*" element={<SignupForm/>}/>
+        <Route path="/about/*" element={<About />} />
+        <Route path="/signup/*" element={<SignupForm newUserSubmitHandler={newUserSubmitHandler} newUserChangeHanldler={newUserChangeHanldler} newUser={newUser}  />} />
       </Routes>
     </div>
   );
-      
-      
+
+
 }
 
 export default App;
--- client/src/Components/TopNav.js
+++ client/src/Components/TopNav.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -10,32 +10,33 @@ import { Link } from "react-router-dom";
 
 
 
-function TopNav() {
+function TopNav({ changeHanldler, submitHandler, loginDetails, user }) {
 
-     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const toggleModal = () => {
-    setIsModalOpen(true)
-  }
+    const toggleModal = () => {
+        setIsModalOpen(true)
+    }
 
     return (
         <div >
             <Box sx={{ flexGrow: 1 }}>
                 <AppBar position="static" id="nav-container">
                     <Toolbar>
-                    <Typography variant="h  6" component="div" sx={{ flexGrow: 1 }}>
-                        App Name and Logo
-                    </Typography>
-                    <Link className="nav-link" to="/"><Button color="inherit">Home</Button></Link>
-                     <Link className="nav-link" to="about"><Button color="inherit">About</Button></Link>
-                     <Link className="nav-link" to="signup"><Button color="inherit">Signup</Button></Link>
-                    <Button onClick={toggleModal}  color="inherit">Login</Button>
+                        <Typography variant="h  6" component="div" sx={{ flexGrow: 1 }}>
+                            App Name and Logo
+                        </Typography>
+                        <Link className="nav-link" to="/"><Button color="inherit">Home</Button></Link>
+                        <Link className="nav-link" to="about"><Button color="inherit">About</Button></Link>
+                        <Link className="nav-link" to="signup"><Button color="inherit">Signup</Button></Link>
+                        <Button onClick={toggleModal} color="inherit">Login</Button>
+                        <p>The user is {user.name}</p>
                     </Toolbar>
                 </AppBar>
             </Box>
-            {isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
-                
-            
+            {isModalOpen && <Modal changeHanldler={changeHanldler} submitHandler={submitHandler} loginDetails={loginDetails} setIsModalOpen={setIsModalOpen} />}
+
+
         </div>
     )
 }
