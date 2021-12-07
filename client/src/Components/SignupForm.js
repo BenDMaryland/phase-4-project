1d502415a7ea99be3c2e0aad721cfbe66c9daa3c
My Name <ben.d.maryland@gmail.com>
2021-12-07 10:39:42 -0500

    Sign up works

--- client/src/Components/SignupForm.js
+++ client/src/Components/SignupForm.js
@@ -6,14 +6,14 @@ function SignupForm({newUserSubmitHandler, newUserChangeHanldler, newUser}) {
         <div className="sign-up-form">
             <AccountCircleIcon id="user-icon"/>
             <h1>Sign Up <br /><span id="fill-out-form">Please fill this form to create an account</span></h1>
-            <form onSubmit={newUserSubmitHandler} action="">
+            <form onSubmit={newUserSubmitHandler} >
                 <input type="name" className="input-box" placeholder="Username" name="name" value={newUser.name} onChange={newUserChangeHanldler}/>
                 <input type="email" className="input-box" placeholder="email" name="email" value={newUser.email} onChange={newUserChangeHanldler}/>
                 <input type="password" className="input-box" placeholder="Password" name="password" value={newUser.password} onChange={newUserChangeHanldler}/>
                 <p> <span><input type="checkbox"/></span> I accept terms and conditions</p>
-                <button type="button" className="sign-btn">Sign up</button>
-                <p> <span><input type="checkbox" onChange={newUserChangeHanldler}/></span>Photographer</p> 
-                <p> <span><input type="checkbox" onChange={newUserChangeHanldler}/></span>Car owner</p> 
+                <button className="sign-btn">Sign up</button>
+                <p> <span><input type="checkbox"   name='photographer' onChange={newUserChangeHanldler}/></span>Photographer</p>
+                <p> <span><input type="checkbox"  onChange={newUserChangeHanldler}/></span>Car owner</p> 
                 <p>Do you have an account? <a href="#" className="sign-in-btn">Sign in</a></p>
             </form>
         </div>
