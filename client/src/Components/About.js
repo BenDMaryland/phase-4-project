bd09ae174278accc7f26dea9f1242cb17fe5eb7a
Dimitri Chenet <dchenet1017@gmail.com>
2021-12-08 17:13:41 -0500

    logo and about

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -9,7 +9,10 @@ function About() {
             </div>
             <div className="about-info">
                 <h4 className="about-sub-header">Who We Are</h4>
-                <p>......</p>
+                <p className="about-sub-head"> Us at Car Media are car enthusiasts that love anything to do with cars. Us at Car Media are the type of people who can talk for hours about cars all day long and not get tired of it.
+                     We encourage creativity and learning and that is what led us to creating this platform. Serving the car enthusiasts community is our main goal and creating a safe place where visitors 
+                     can upload photos of their cars, get likes and also have people give opinions on the vehicle they like or don’t sounds like a dream. So we created it !
+</p>
             </div>
         </div>
     )
